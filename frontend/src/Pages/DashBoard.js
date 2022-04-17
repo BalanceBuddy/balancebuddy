e18f988b21d3f2c102178612e40d9c4ca71fc2fa
Tasheem Brown <tasheembrown@gmail.com>
2022-04-17 07:44:47 -0400

    rough porgress bar + slideshow

--- frontend/src/Pages/DashBoard.js
+++ frontend/src/Pages/DashBoard.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-import Carousel from 'react-bootstrap/Carousel';
-import { Slide } from 'react-slideshow-image';
-import Slideshow from './Slideshow';
-import DefaultContainer from '../Components/DefaultContainer';
+import React from "react";
+import styled from "styled-components";
+
+import DefaultContainer from "../Components/DefaultContainer";
+
+import CarouselSlide from "../Components/CarouselSlide"
+import ProgressBar from "../Components/ProgressBar";
+import Button from "../Components/Button";
+
 
 const Row = styled.div`
   display: flex;
@@ -12,18 +15,10 @@ const Row = styled.div`
   display: flex;
   justify-content: center; // centers rodamap text
 `;
-// Motivation Board
-const slideImages = [
-  '../Assets/motivation/puppy.jpeg',
-  '../Assets/motivation/love.jpeg',
-  '../Assets/motivation/mansion.jpeg',
-  '../Assets/motivation/goggins.jpeg',
-];
-
-const properties = {};
 
 const Col = styled.div`
   padding-left: 30px;
+  padding-right: 20px;
   text-align: left;
   // width: 45%;
   border-radius: 5px;
@@ -38,12 +33,11 @@ const Col = styled.div`
   }
 
   h2 {
-    font-size: 32px;
+    font-size: 26px;
     margin-bottom: 0px;
   }
-  p {
-    font-size: 18px;
-  }
+
+
   .text {
     padding-right: 0px;
     margin-top: 10px;
@@ -52,35 +46,13 @@ const Col = styled.div`
   .above {
     margin-top: 0px;
   }
-  .button {
-    background-color: #62bdca;
-    border: none;
 
-    filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.25));
-  }
 
   button {
-    background-color: #62bdca;
-    font-size: 18px;
-    text-align: left;
-    /* Auto layout */
-
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    padding: 10px;
-
-    width: 200px;
+    width: 144px;
     height: 42px;
-    left: 480px;
-    top: 162px;
-    margin: 10px;
-
-    background: #ffffff;
-    border: 1px solid #000000;
-    box-sizing: border-box;
-    border-radius: 10px;
+    margin: 8px;
+    font-size: 16px;
   }
 
   .task-container {
@@ -88,58 +60,55 @@ const Col = styled.div`
     flex-direction: row;
   }
   .task-item {
-    padding-right: 40px;
+   // padding-right: 20px;
     padding-top: 30px;
   }
-  .resource-item {
-    padding: 40px;
-  }
-
-  // slide show
 `;
-
-const now = 60;
 
 function DashBoard() {
   return (
     <DefaultContainer>
-      <h1>Dashboard</h1>
+      {/* <h1>Dashboard</h1> */}
       <Row>
         <Col>
-          <div className='task-container'>
+          <div className="task-container">
             <div>
               <h2>Current Task</h2>
-              <p className='text'>
+              <p className="text">
                 Update resume so that all recent experiences have been added.
                 Ensure that descriptions include key action words and can be
                 quantifiable.
               </p>
             </div>
 
-            <div className='task-item'>
-              <button type='button' className='button'>
-                Completed Task
-              </button>
-              <button type='button' className='button'>
-                Change Task
-              </button>
+            <div className="task-item">
+              <Button>Task Completed</Button>
+              <Button>Change Task</Button>
             </div>
           </div>
-          <div className='task-container'>
+          <div className="task-container">
             <div>
-              <h2 className='above'>Resources</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                viverra lectus.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                viverra lectus.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                viverra lectus.
-              </p>
+              <h2 className="above">Resources</h2>
+              <div>
+                <p>
+                  <a href="https://www.w3schools.com/">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aliquam viverra lectus.
+                  </a>
+                </p>
+                <p>
+                  <a href="https://www.w3schools.com/">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aliquam viverra lectus.
+                  </a>
+                </p>
+                <p>
+                  <a href="https://www.w3schools.com/">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aliquam viverra lectus.
+                  </a>
+                </p>
+              </div>
             </div>
             <div></div>
           </div>
@@ -148,55 +117,17 @@ function DashBoard() {
         <Col>
           <h2>Motivation</h2>
           <div>
-            <Carousel>
-              <Carousel.Item>
-                <img
-                  className='d-block w-100'
-                  src='holder.js/800x400?text=First slide&bg=373940'
-                  alt='First slide'
-                />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className='d-block w-100'
-                  src='holder.js/800x400?text=Second slide&bg=282c34'
-                  alt='Second slide'
-                />
-
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className='d-block w-100'
-                  src='holder.js/800x400?text=Third slide&bg=20232a'
-                  alt='Third slide'
-                />
-
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl
-                    consectetur.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
+            <CarouselSlide
+              content={{ backgroundColor: "#ff7c7c", title: "Slide 1" }}
+            />
           </div>
         </Col>
       </Row>
       <Row>
-        <h1>Roadmap</h1>
+        <div>
+          <h1>Roadmap</h1>
+          <ProgressBar />
+        </div>
       </Row>
     </DefaultContainer>
   );
