2259f5f3d25515496fc5c3551f9e587044d7505d
Tasheem Brown <tasheembrown@gmail.com>
2022-04-17 07:50:13 -0400

    updated motivation board

--- frontend/src/Pages/DashBoard.js
+++ frontend/src/Pages/DashBoard.js
@@ -3,10 +3,9 @@ import styled from "styled-components";
 
 import DefaultContainer from "../Components/DefaultContainer";
 
-import CarouselSlide from "../Components/CarouselSlide"
+import CarouselSlide from "../Components/CarouselSlide";
 import ProgressBar from "../Components/ProgressBar";
 import Button from "../Components/Button";
-
 
 const Row = styled.div`
   display: flex;
@@ -65,6 +64,18 @@ const Col = styled.div`
   }
 `;
 
+// Slides
+
+export const SLIDE_INFO = [
+  { backgroundColor: "#ff7c7c", title: "Slide 1" },
+  { backgroundColor: "#ffb6b9", title: "Slide 2" },
+  { backgroundColor: "#8deaff", title: "Slide 3" },
+  { backgroundColor: "#ffe084", title: "Slide 4" },
+  { backgroundColor: "#d9d9d9", title: "Slide 5" },
+];
+
+const content = SLIDE_INFO[3];
+
 function DashBoard() {
   return (
     <DefaultContainer>
@@ -117,9 +128,7 @@ function DashBoard() {
         <Col>
           <h2>Motivation</h2>
           <div>
-            <CarouselSlide
-              content={{ backgroundColor: "#ff7c7c", title: "Slide 1" }}
-            />
+            <CarouselSlide content={content} />
           </div>
         </Col>
       </Row>
