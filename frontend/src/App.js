b0b1369586381acc77a89fd7bce613ba3fa60dd3
Tasheem Brown <tasheembrown@gmail.com>
2022-04-17 01:16:06 -0400

    new components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Settings from "./Pages/Settings";
 import { Routes, Route, Link } from "react-router-dom";
 import styled from "styled-components";
 
+<<<<<<< Updated upstream
 const MainContainer = styled.div`
   padding-top: 40px;
   padding-bottom: 80px;
@@ -14,6 +15,19 @@ const MainContainer = styled.div`
   margin: auto;
   max-width: 100%;
 `;
+=======
+import React, { Component }  from 'react';
+
+
+import Signup from './Pages/Signup/Signup';
+import CalendarAccess from './Pages/Signup/CalendarAccess';
+import CalendarAccepted from './Pages/Signup/CalendarAccepted';
+import CalendarDenied from './Pages/Signup/CalendarDenied';
+import AssessmentPageOne from './Pages/Signup/AssessmentPageOne';
+import AssessmentPageTwo from './Pages/Signup/AssessmentPageTwo';
+import LoadingDashboard from './Pages/Signup/LoadingDashboard';
+import { Routes, Route } from 'react-router-dom';
+>>>>>>> Stashed changes
 
 function App() {
   return (
--- frontend/src/Pages/DashBoard.js
+++ frontend/src/Pages/DashBoard.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useState } from "react";
 import { useEffect } from "react";
 import styled from "styled-components";
@@ -200,7 +201,47 @@ function DashBoard() {
         <h1>Roadmap</h1>
       </Row>
     </>
+=======
+import "./App.css";
+import DashBoard from "./Pages/DashBoard";
+import Settings from "./Pages/Settings";
+import Login from "./Pages/Login";
+
+import Signup from "./Pages/Signup/Signup";
+import CalendarAccess from "./Pages/Signup/CalendarAccess";
+import CalendarAccepted from "./Pages/Signup/CalendarAccepted";
+import CalendarDenied from "./Pages/Signup/CalendarDenied";
+import AssessmentPageOne from "./Pages/Signup/AssessmentPageOne";
+import AssessmentPageTwo from "./Pages/Signup/AssessmentPageTwo";
+import LoadingDashboard from "./Pages/Signup/LoadingDashboard";
+
+import React, { Component }  from 'react';
+
+
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<DashBoard />} />
+        <Route path="/settings" element={<Settings />} />
+
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/calendar-access" element={<CalendarAccess />} />
+        <Route
+          path="/calendar-access/accepted"
+          element={<CalendarAccepted />}
+        />
+        <Route path="/calendar-access/denied" element={<CalendarDenied />} />
+        <Route path="/assessment/1" element={<AssessmentPageOne />} />
+        <Route path="/assessment/2" element={<AssessmentPageTwo />} />
+        <Route path="/loading-dashboard" element={<LoadingDashboard />} />
+      </Routes>
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
-export default DashBoard;
+export default App;
